23c082a4ef8f27c16887638717a63dd51529e299
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-06-22 19:51:12 +0600

    learnt morph plugin

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ document.addEventListener('alpine:init', () => {
     username: 'thesalahrand',
     posts: ['Post 01', 'Post 02']
   });
+
+  document.querySelector('.run-morph').addEventListener('click', () => {
+    const el = document.querySelector('.morph-wrapper');
+
+    Alpine.morph(el, `
+      <div class="morph-wrapper" x-data="{ message: 'Change me, then press the button!' }">
+        <h2>See how elements have been added</h2>  
+
+        <input type="text" x-model="message" class="border">
+        <span x-text="message"></span>
+
+        <h2>but the state of this component hasn't changed? Magical</h2>
+      </div>
+    `);
+  });
 });
